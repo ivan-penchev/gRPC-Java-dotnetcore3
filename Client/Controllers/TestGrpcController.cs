1ba0b10c5745c4bd4576be6e422314e875a7ba7a
ivanspenchev <ivan@saphe.dk>
2019-10-01 13:11:49 +0200

    Added dotnet client that is web accessible

---
+++ Client/Controllers/TestGrpcController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestGrpcController : ControllerBase
+    {
+     
+
+        public TestGrpcController()
+        {
+       
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("http://docker.for.win.localhost:50052");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            return Ok(reply.Message);
+        }
+    }
+}
